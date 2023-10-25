41110deb1600cf459124a53d5f3d1f3d4b709d0f
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-10-25 11:17:24 -0600

    add crud and querys book

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,77 +1,267 @@
-const dynamoDB = require('../database');
+const { MongoClient, ObjectId } = require('mongodb')
 const fs = require('fs');
 const csv = require('csv-parser');
 
+const uri = 'mongodb+srv://user-practica-3:gAxNX07uoh8IdXl1@cluster0.smqaw64.mongodb.net/?retryWrites=true&w=majority'
 class booksController {
   constructor() { }
 
   async loadBooksData(req, res) {
     try {
-      const tableName = 'libros';
-      const items = [];
-      var cont = 1;
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
 
-      fs.createReadStream('./data/Libros.csv')
+      const books = database.collection('books');
+
+      const filePath = './data/Libros.csv';
+
+      fs.createReadStream(filePath)
         .pipe(csv())
-        .on('data', (row) => {
-          console.log(row)
-          items.push({
-            Id: Number(cont),
-            Titulo: row.Titulo,
-            Autor: row.Autor,
-            Descripcion: row.Descripcion,
-            FechaDePublicacion: row.FechaDePublicacion,
-            Calificacion: row.Calificacion,
-            Stock: Number(row.Stock),
-            Categoria: row.Categoria,
-          });
-          cont++;
+        .on('data', async row => {
+          await books.insertOne(row);
         })
         .on('end', () => {
-          const putRequests = items.map((item) => ({
-            PutRequest: {
-              Item: item,
-            },
-          }));
-
-          const params = {
-            RequestItems: {
-              [tableName]: putRequests,
-            },
-          };
-
-          dynamoDB.batchWrite(params, (error, data) => {
-            if (error) {
-              console.error(`Error al cargar datos en la tabla ${tableName}:`, error);
-              res.status(500).json({ error: `Error al cargar datos en la tabla ${tableName}` });
-            } else {
-              console.log(`Datos de ${tableName} cargados con éxito`);
-              res.status(200).json({ message: `Datos de ${tableName} cargados con éxito` });
-            }
-          });
+          console.log('CSV file successfully processed');
+          res.send('CSV file successfully processed');
         });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
+
+    } catch (error) {
+      console.error('Error de conexión a MongoDB:', error);
+      res.status(500).send(error)
+    } finally {      
+      //await client.close();
     }
   }
 
   async getBooks(req, res) {
     try {
-      const params = {
-        TableName: 'libros',
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const query = {};
+
+      const booksList = await books.find(query).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async createBook(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const book = req.body;
+
+      await books.insertOne(book);
+
+      res.status(200).send('Book created successfully');
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async updateBook(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect()
+
+      const database = client.db('practica3');
+      const books = database.collection('books');
+      
+      const { _id, 
+        Calificacion,
+        Stock,
+        Titulo,
+        Autor,
+        Descripcion,
+        FechaDePublicacion,
+        Categoria } = req.body;
+
+      const query = { _id: new ObjectId(_id) };
+
+      const newBook = {
+        Calificacion,
+        Stock,
+        Titulo,
+        Autor,
+        Descripcion,
+        FechaDePublicacion,
+        Categoria
       };
 
-      dynamoDB.scan(params, (error, data) => {
-        if (error) {
-          res.status(500).json({ error: 'Error al obtener datos de libros' });
-        } else {
-          res.json(data.Items);
-        }
-      });
+      await books.updateOne(query, { $set: newBook });
+
+      res.status(200).send('Book updated successfully');
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async deleteBook(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect()
+
+      const database = client.db('practica3');
+      const books = database.collection('books');
+      
+      const { _id } = req.params;
+
+      const query = { _id: new ObjectId(_id) };
+
+      await books.deleteOne(query);
+
+      res.status(200).send('Book deleted successfully');
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async getAvailableBooks(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const query = { Stock: { $gt: "0" } };
+
+      const booksList = await books.find(query).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async findByCategory(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const { category } = req.params;
+
+      const query = { Categoria: category };
+
+      const booksList = await books.find(query).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async findByAuthor(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const { author } = req.params;
+
+      const query = { Autor: author };
+
+      const booksList = await books.find(query).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async averageRatingSort(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const query = {};
+
+      const booksList = await books.find(query).sort({ Calificacion: -1 }).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async priceLowerThan20(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const query = { Precio: { $lt: "20" } };
+
+      const booksList = await books.find(query).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
+    }
+  }
+
+  async titleAndDescriptionKeyword(req, res) {
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      console.log('Conexión a MongoDB establecida correctamente');
+      
+      const database = client.db('practica3');
+
+      const books = database.collection('books');
+
+      const { keyword } = req.params;
+
+      const query = { $or: [
+        { Titulo: { $regex: keyword, $options: 'i' } },
+        { Descripcion: { $regex: keyword, $options: 'i' } }
+      ] };
+
+      const booksList = await books.find(query).toArray();
+      res.status(200).send(booksList);
+    } catch (err) {
+      console.error('Error de conexión a MongoDB:', err);
+      res.status(500).send(err)
     }
   }
 }
--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/booksController');
 
-router.get('/', booksController.getBooks);
+//router.get('/', booksController.getBooks);
 router.get('/load', booksController.loadBooksData);
+
+router.get('/', booksController.getBooks);
+router.post('/', booksController.createBook);
+router.put('/', booksController.updateBook);
+router.delete('/:_id', booksController.deleteBook);
+
+router.get('/available', booksController.getAvailableBooks);
+router.get('/category/:category', booksController.findByCategory);
+router.get('/author/:author', booksController.findByAuthor);
+router.get('/averageRatingSort', booksController.averageRatingSort);
+router.get('/priceLowerThan20', booksController.priceLowerThan20);
+router.get('/titleAndDescriptionKeyword/:keyword', booksController.titleAndDescriptionKeyword);
 
 module.exports = router;
